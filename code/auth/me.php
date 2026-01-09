7a870f97ee0a60221cdc93bb2123f0de00af20f9
Morenikeji-Olowo <morenikejiolowo101@gmail.com>
2026-01-09 15:46:12 +0100

    fixed

--- code/auth/me.php
+++ code/auth/me.php
@@ -1,7 +1,7 @@
 <?php
 include '../../config/db.php';
 
-$frontend_origin = "https://aurainvestfront-o4ksfwed4-morenikeji-olowos-projects.vercel.app";
+$frontend_origin = "https://aurainvestfront-q81d5r6vu-morenikeji-olowos-projects.vercel.app";
 
 header("Access-Control-Allow-Origin: $frontend_origin");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
--- code/onBoarding/save.php
+++ code/onBoarding/save.php
@@ -2,7 +2,7 @@
 include '../../config/db.php';
 session_start();
 
-$frontend_origin = "https://aurainvestfront-o4ksfwed4-morenikeji-olowos-projects.vercel.app";
+$frontend_origin = "https://aurainvestfront-q81d5r6vu-morenikeji-olowos-projects.vercel.app";
 
 header("Access-Control-Allow-Origin: $frontend_origin");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
