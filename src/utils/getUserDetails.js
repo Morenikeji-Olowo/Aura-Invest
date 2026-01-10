46868535597dddede0a7f0be1ce943d34e6cdb2c
Morenikeji-Olowo <morenikejiolowo101@gmail.com>
2026-01-10 22:41:33 +0100

    FIXING LAYOUT ISSUESssss

--- src/utils/getUserDetails.js
+++ src/utils/getUserDetails.js
@@ -12,7 +12,7 @@ export async function getUserProfile() {
 
     try {
         const response = await fetch(
-            `${API_BASE_URL}/profile/getUserProfile.php`,
+            `${API_BASE_URL}/code/profile/getUserProfile.php`,
             {
                 method: "GET",
                 credentials: "include", 
